9dd541bd96f6e576e891321d94fbde1b7a5f2a17
ParagWankhade <iamprg@gmail.com>
2021-01-19 18:42:59 +0530

    Integration test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var url = "mongodb://localhost:27017/mydb?replicaSet=rs0";
+var url = "mongodb://"+process.env.MONGO_URL+":27017/mydb?replicaSet=rs0";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
